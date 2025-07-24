5b6d1a105170fa459e1c074cb4944b4a3fc0fa9b
Megitsune <megitsune14.pro@gmail.com>
2025-07-24 18:03:14 +0200

    Update

--- decryptage/settings.py
+++ decryptage/settings.py
@@ -77,5 +77,6 @@ USE_TZ = True
 # --- Fichiers statiques ---
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
